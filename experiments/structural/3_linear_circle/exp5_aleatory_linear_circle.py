282288dcda6a486b3bb75b3dcd1779c2a2eb5925
carlos <crptcusco@gmail.com>
2024-02-27 12:35:19 -0300

    incremental

--- experiments/structural/3_linear_circle/exp5_aleatory_linear_circle.py
+++ experiments/structural/3_linear_circle/exp5_aleatory_linear_circle.py
@@ -2,7 +2,6 @@
 import os
 import time
 import pandas as pd
-from memory_profiler import profile
 import pickle
 
 # local imports
@@ -14,133 +13,131 @@ Experiment 5 - Test the path and 2_ring structures
 using aleatory generated template for the local network 
 """
 
+# experiment parameters
+N_SAMPLES = 10
+N_LOCAL_NETWORKS_MIN = 9
+N_LOCAL_NETWORKS_MAX = 9
+N_VAR_NETWORK = 5
+N_OUTPUT_VARIABLES = 2
+N_INPUT_VARIABLES = 2
+L_TOPOLOGIES = [4, 3]
+N_CLAUSES_FUNCTION = 2
 
-@profile
-def run_script():
-    # experiment parameters
-    N_SAMPLES = 10
-    N_LOCAL_NETWORKS_MIN = 9
-    N_LOCAL_NETWORKS_MAX = 9
-    N_VAR_NETWORK = 5
-    N_OUTPUT_VARIABLES = 2
-    N_INPUT_VARIABLES = 2
-    # V_TOPOLOGY = 3  # cycle graph
-    N_CLAUSES_FUNCTION = 2
-    N_DIRECTED_EDGES = 1
+# verbose parameters
+SHOW_MESSAGES = True
 
-    # verbose parameters
-    SHOW_MESSAGES = True
+# Begin the Experiment
+print("BEGIN THE EXPERIMENT")
+print("="*80)
 
-    # Begin the Experiment
-    # Capture the time for all the experiment
-    v_begin_exp = time.time()
+# Capture the time for all the experiment
+v_begin_exp = time.time()
 
-    # Experiment Name
-    experiment_name = "exp5_aleatory_linear_circle"
+# Experiment Name
+EXPERIMENT_NAME = "exp5_aleatory_linear_circle"
 
-    # Create the 'outputs' directory if it doesn't exist
-    output_folder = 'outputs'
-    os.makedirs(output_folder, exist_ok=True)
+# Create the 'outputs' directory if it doesn't exist
+OUTPUT_FOLDER = 'outputs'
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-    # create an experiment directory by parameters
-    directory_path = (output_folder + "/" + experiment_name + "_"
-                      + str(N_LOCAL_NETWORKS_MIN) + "_"
-                      + str(N_LOCAL_NETWORKS_MAX)
-                      + "_" + str(N_SAMPLES))
-    os.makedirs(directory_path, exist_ok=True)
+# create an experiment directory by parameters
+DIRECTORY_PATH = (OUTPUT_FOLDER + "/" + EXPERIMENT_NAME + "_"
+                  + str(N_LOCAL_NETWORKS_MIN) + "_"
+                  + str(N_LOCAL_NETWORKS_MAX)
+                  + "_" + str(N_SAMPLES))
+os.makedirs(DIRECTORY_PATH, exist_ok=True)
 
-    # generate the experiment data file in csv
-    file_path = directory_path + '/data.csv'
+# generate the experiment data file in csv
+file_path = DIRECTORY_PATH + '/data.csv'
 
-    # Erase the file if exists
-    if os.path.exists(file_path):
-        os.remove(file_path)
-        print("Existing file deleted:", file_path)
+# Erase the file if exists
+if os.path.exists(file_path):
+    os.remove(file_path)
+    print("Existing file deleted:", file_path)
 
-    # Begin the process
-    for n_local_networks in range(N_LOCAL_NETWORKS_MIN, N_LOCAL_NETWORKS_MAX + 1):  # 5
-        for i_sample in range(1, N_SAMPLES + 1):  # 1 - 1000 , 1, 2
-            # generate the aleatory local network template
-            o_path_circle_template = PathCircleTemplate.generate_aleatory_template(
-                n_var_network=N_VAR_NETWORK, n_input_variables=2)
-            for v_topology in [4, 3]:
-                l_data_sample = []
-                print("Experiment", i_sample, "of", N_SAMPLES, " TOPOLOGY:", v_topology)
+# Begin the process
+for n_local_networks in range(N_LOCAL_NETWORKS_MIN, N_LOCAL_NETWORKS_MAX + 1):  # 5
+    for i_sample in range(1, N_SAMPLES + 1):  # 1 - 1000 , 1, 2
+        # generate the aleatory local network template
+        o_path_circle_template = PathCircleTemplate.generate_aleatory_template(
+            n_var_network=N_VAR_NETWORK, n_input_variables=N_INPUT_VARIABLES)
+        for i_topology in L_TOPOLOGIES:
+            l_data_sample = []
+            print("Experiment", i_sample, "of", N_SAMPLES, " TOPOLOGY:", i_topology)
 
-                o_cbn = o_path_circle_template.generate_cbn_from_template(v_topology=v_topology,
-                                                                          n_local_networks=n_local_networks)
+            o_cbn = o_path_circle_template.generate_cbn_from_template(v_topology=i_topology,
+                                                                      n_local_networks=n_local_networks)
 
-                # find attractors
-                v_begin_find_attractors = time.time()
-                o_cbn.find_local_attractors_optimized()
-                v_end_find_attractors = time.time()
-                n_time_find_attractors = v_end_find_attractors - v_begin_find_attractors
+            # find attractors
+            v_begin_find_attractors = time.time()
+            o_cbn.find_local_attractors_optimized()
+            v_end_find_attractors = time.time()
+            n_time_find_attractors = v_end_find_attractors - v_begin_find_attractors
 
-                # find the compatible pairs
-                v_begin_find_pairs = time.time()
-                o_cbn.find_compatible_pairs()
-                v_end_find_pairs = time.time()
-                n_time_find_pairs = v_end_find_pairs - v_begin_find_pairs
+            # find the compatible pairs
+            v_begin_find_pairs = time.time()
+            o_cbn.find_compatible_pairs()
+            v_end_find_pairs = time.time()
+            n_time_find_pairs = v_end_find_pairs - v_begin_find_pairs
 
-                # Find attractor fields
-                v_begin_find_fields = time.time()
-                o_cbn.find_attractor_fields()
-                v_end_find_fields = time.time()
-                n_time_find_fields = v_end_find_fields - v_begin_find_fields
+            # Find attractor fields
+            v_begin_find_fields = time.time()
+            o_cbn.find_attractor_fields()
+            v_end_find_fields = time.time()
+            n_time_find_fields = v_end_find_fields - v_begin_find_fields
 
-                # collect indicators
-                d_collect_indicators = {
-                    # initial parameters
-                    "i_sample": i_sample,
-                    "n_local_networks": n_local_networks,
-                    "n_var_network": N_VAR_NETWORK,
-                    "v_topology": v_topology,
-                    "n_output_variables": N_OUTPUT_VARIABLES,
-                    "n_clauses_function": N_CLAUSES_FUNCTION,
-                    # calculate parameters
-                    "n_local_attractors": o_cbn.get_n_local_attractors(),
-                    "n_pair_attractors": o_cbn.get_n_pair_attractors(),
-                    "n_attractor_fields": o_cbn.get_n_attractor_fields(),
-                    # time parameters
-                    "n_time_find_attractors": n_time_find_attractors,
-                    "n_time_find_pairs": n_time_find_pairs,
-                    "n_time_find_fields": n_time_find_fields
-                }
-                l_data_sample.append(d_collect_indicators)
+            # collect indicators
+            d_collect_indicators = {
+                # initial parameters
+                "i_sample": i_sample,
+                "n_local_networks": n_local_networks,
+                "n_var_network": N_VAR_NETWORK,
+                "v_topology": i_topology,
+                "n_output_variables": N_OUTPUT_VARIABLES,
+                "n_clauses_function": N_CLAUSES_FUNCTION,
+                # calculate parameters
+                "n_local_attractors": o_cbn.get_n_local_attractors(),
+                "n_pair_attractors": o_cbn.get_n_pair_attractors(),
+                "n_attractor_fields": o_cbn.get_n_attractor_fields(),
+                # time parameters
+                "n_time_find_attractors": n_time_find_attractors,
+                "n_time_find_pairs": n_time_find_pairs,
+                "n_time_find_fields": n_time_find_fields
+            }
+            l_data_sample.append(d_collect_indicators)
 
-                # save the collected indicator to analysis
-                pf_res = pd.DataFrame(l_data_sample)
-                pf_res.reset_index(drop=True, inplace=True)
+            # save the collected indicator to analysis
+            pf_res = pd.DataFrame(l_data_sample)
+            pf_res.reset_index(drop=True, inplace=True)
 
-                # if the file exist, open the 'a' mode (append), else create a new file
-                mode = 'a' if os.path.exists(file_path) else 'w'
-                # Add the header only if is a new file
-                header = not os.path.exists(file_path)
-                #  save the data in csv file
-                pf_res.to_csv(file_path, mode=mode, header=header, index=False)
+            # if the file exist, open the 'a' mode (append), else create a new file
+            mode = 'a' if os.path.exists(file_path) else 'w'
+            # Add the header only if is a new file
+            header = not os.path.exists(file_path)
+            #  save the data in csv file
+            pf_res.to_csv(file_path, mode=mode, header=header, index=False)
 
-                print("Experiment data saved in:", file_path)
+            print("Experiment data saved in:", file_path)
 
-                # Open a file in binary write mode (wb)
-                pickle_path = directory_path + '/cbn_' + str(i_sample) + '_' + str(v_topology) + ".pkl"
-                with open(pickle_path, 'wb') as file:
-                    # Use pickle.dump to save the object to the file
-                    pickle.dump(o_cbn, file)
+            # Open a file in binary write mode (wb)
+            pickle_path = DIRECTORY_PATH + '/cbn_' + str(i_sample) + '_' + str(i_topology) + ".pkl"
+            with open(pickle_path, 'wb') as file:
+                # Use pickle.dump to save the object to the file
+                pickle.dump(o_cbn, file)
 
-                # Close the file
-                file.close()
-                print("Pickle object saved in:", pickle_path)
+            # Close the file
+            file.close()
+            print("Pickle object saved in:", pickle_path)
 
-                CustomText.print_duplex_line()
-            CustomText.print_stars()
-        CustomText.print_dollars()
+            CustomText.print_duplex_line()
+        CustomText.print_stars()
+    CustomText.print_dollars()
 
-    # Take the time of the experiment
-    v_end_exp = time.time()
-    v_time_exp = v_end_exp - v_begin_exp
-    print("Time experiment (in seconds): ", v_time_exp)
+# Take the time of the experiment
+v_end_exp = time.time()
+v_time_exp = v_end_exp - v_begin_exp
+print("Time experiment (in seconds): ", v_time_exp)
 
-    print("End experiment")
+print("="*80)
+print("END EXPERIMENT")
 
-
-run_script()
