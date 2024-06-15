1f6feef83406a4339eaec98a03a17c294d8cb946
crptcusco <crptcusco@gmail.com>
2024-06-15 13:13:21 -0300

    incremental

--- experiments/7_aleatory_template_coupling_signals/exp7_generator.py
+++ experiments/7_aleatory_template_coupling_signals/exp7_generator.py
@@ -13,7 +13,7 @@ Experiment 7 - Test the aleatory CBNs with different number coupling signals
 """
 
 # experiment parameters
-N_SAMPLES = 10
+N_SAMPLES = 100
 N_LOCAL_NETWORKS_MIN = 3
 N_LOCAL_NETWORKS_MAX = 9
 N_VAR_NETWORK = 5
@@ -65,7 +65,7 @@ for i_sample in range(1, N_SAMPLES + 1):  # 1 - 1000 , 1, 2
     o_topology_template = AleatoryTemplate.generate_aleatory_template(n_var_network=N_VAR_NETWORK,
                                                                       v_topology=V_TOPOLOGY)
     for n_local_networks in range(N_LOCAL_NETWORKS_MIN, N_LOCAL_NETWORKS_MAX + 1):
-        for n_edges in range(n_local_networks, n_local_networks*2+1):
+        for n_edges in range(n_local_networks, (n_local_networks*2)+1):
             l_data_sample = []
             print("Experiment", i_sample, "of", N_SAMPLES, " TOPOLOGY:", V_TOPOLOGY)
 
