dc63bf91b50087dc97c4639405c480c0ff727926
Carlos R P Tovar <crptcusco@gmail.com>
2024-06-28 21:15:46 -0300

    incremental

--- experiments/2_networks_circular/submit_sdumont_exp2.sh
+++ experiments/2_networks_circular/submit_sdumont_exp2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=3            # Número de Nós
-#SBATCH --ntasks-per-node=16 # Número de tarefas por Nó
-#SBATCH --ntasks=48          # Número total de tarefas
+#SBATCH --ntasks-per-node=6 # Número de tarefas por Nó
+#SBATCH --ntasks=18          # Número total de tarefas
 #SBATCH -p cpu_long          # Fila (partition) a ser utilizada
 #SBATCH -J CRPT              # Nome do job
 #SBATCH --time=48:00:00      # Tempo máximo de execução
