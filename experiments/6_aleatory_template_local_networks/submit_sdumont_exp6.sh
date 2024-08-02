fac6c458735f0aa703f35ff28629223c466a959c
Carlos R P Tovar <crptcusco@gmail.com>
2024-08-02 16:32:05 -0300

    incremental

--- experiments/6_aleatory_template_local_networks/submit_sdumont_exp6.sh
+++ experiments/6_aleatory_template_local_networks/submit_sdumont_exp6.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=2            # Número de Nós
 #SBATCH --ntasks-per-node=3  # Número de tarefas por Nó
 #SBATCH --ntasks=6          # Número total de tarefas
-#SBATCH -p cpu_shared        # Fila (partition) a ser utilizada
+#SBATCH -p cpu_small        # Fila (partition) a ser utilizada
 #SBATCH -J CRPT              # Nome do job
 #SBATCH --time=48:00:00      # Tempo máximo de execução
 #SBATCH -e log/slurm-%j.err  # Arquivo de erro
