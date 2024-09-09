7a626f1db9e33b72b7e2923f4abbdc0801ada30e
carlos <crptcusco@gmail.com>
2024-09-09 02:16:17 -0300

    incremental

--- experiments/7_aleatory_template_coupling_signals/outputs/exp7_data_7_8_1000/union_data.py
+++ experiments/7_aleatory_template_coupling_signals/outputs/exp7_data_7_8_1000/union_data.py
@@ -6,8 +6,8 @@ df2 = pd.read_csv('data_270.csv').iloc[:179 * 9]   # Hacemos lo mismo aquí
 df3 = pd.read_csv('data_100.csv')                  # Este archivo está completo
 
 # Ajustar el índice 'i_sample' en cada DataFrame
-df2['i_sample'] += df1['i_sample'].max() + 1
-df3['i_sample'] += df2['i_sample'].max() + 1
+df2['i_sample'] += 731
+df3['i_sample'] += 731 + 179
 
 # Combinar los tres DataFrames en uno solo
 df_combined = pd.concat([df1, df2, df3], ignore_index=True)
@@ -21,7 +21,7 @@ print(f"Total de registros en el DataFrame combinado: {total_registros}")
 
 # Identificar las muestras únicas y eliminarlas desde el final para quedarnos con 1000
 unique_samples = df_combined['i_sample'].unique()  # Obtener muestras únicas
-samples_to_keep = unique_samples[:-12]  # Mantener todas menos las últimas 12
+samples_to_keep = unique_samples[:-10]  # Mantener todas menos las últimas 12
 
 # Filtrar el DataFrame para quedarse solo con las muestras deseadas
 df_filtered = df_combined[df_combined['i_sample'].isin(samples_to_keep)]
@@ -29,4 +29,4 @@ df_filtered = df_combined[df_combined['i_sample'].isin(samples_to_keep)]
 # Guardar el DataFrame filtrado en un nuevo archivo CSV
 df_filtered.to_csv('data.csv', index=False)
 
-print("Los últimos 12 samples han sido eliminados. Archivo guardado como 'data.csv'.")
+print("Los últimos 10 samples han sido eliminados. Archivo guardado como 'data.csv'.")
